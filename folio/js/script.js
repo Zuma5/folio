0c018d6024726343f314661b60236f7a66a83b48
Zuma5 <Ozi.Zimina@gmail.com>
2024-03-17 04:26:09 +0200

    added project

---
+++ folio/js/script.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function() {
+    $('.details').on('click', '.pop_up', function() {
+        $(this).closest("li").find('.zoom_img').fadeIn(400);
+    });
+    $('.zoom_img').on('click', function() {
+        $(this).fadeOut(400);
+    });
+    
+    $(".nav").on("click", "a", function(event) {
+        event.preventDefault();
+
+        var id = $(this).attr("href"),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+    
+    if(window.matchMedia('(max-width: 701px)').matches) {
+        $("nav").on("click", ".fa", function() {
+            $(".nav").slideToggle("slow");
+            $(".nav").css({display:"flex"});
+        });
+    };
+});
